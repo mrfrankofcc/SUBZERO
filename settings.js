1b836ae418b9629c3403e98391f1d36ab5abbdf0
khuram787 <khuramssultan@gmail.com>
2025-08-05 12:38:42 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;aN8DgYiA#jn0ipj-XnkgszyQcG8JvCV232WAkabVwg-eJL1Ki6II", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "923282494408", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
